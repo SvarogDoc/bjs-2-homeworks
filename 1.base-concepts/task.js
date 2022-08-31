255a5d757a5148be0c0de19e6f1c86eb64f94db4
SvarogDoc <learnqai33@gmail.com>
2022-08-31 23:04:07 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,31 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+
+  // Формула дискриминанта
+
+  let D = Math.pow(b, 2) - (4 * a * c);
+
+  // При дискриминанте >0 вычисляем х1 и х2
+
+  if (D > 0) {
+    let x1 = (-b + Math.sqrt(D)) / (2 * a);
+    let x2 = (-b - Math.sqrt(D)) / (2 * a)
+    arr.push(x1, x2)
+
+   // При дискриминате =0 вычисляем х
+
+  } else if (D===0) {
+    let x = - b / (2 * a)
+    arr.push(x)
+
+   // При дискриминанте 0< корней нет
+   
+  } else if (D < 0) {
+    arr.push()
+  }
+  
   return arr; // array
 }
 
