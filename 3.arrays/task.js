ac8dbd1c96118962d2061e88b1e0248135b3f00c
SvarogDoc <learnqai33@gmail.com>
2022-09-15 17:13:27 +0300

    Initisl commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
+  let result = (arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]));;
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  
+  let resultArr = arr.filter((positive) => positive > 0)
+  .filter((multiple) => multiple % 3 === 0)
+  .map((multiplied) => multiplied * 10);
 
   return resultArr; // array
 }
